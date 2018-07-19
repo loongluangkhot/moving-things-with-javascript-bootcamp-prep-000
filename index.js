6e99e9c8ecd1af3af8a83c31e0c6adfea36423aa
Nalongsak Luangkhot <loongluangkhot@gmail.com>
2018-07-19 17:48:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const app = "I don't do much."
 
+const keyWhichDict = {
+  left: 37,
+  up: 38,
+  right: 39,
+  down: 40
+}
+
+
 document.addEventListener('keydown', function(e) {
-  if (e.which === 37) {
-    moveLeft()
-  } else (e.which === )
+  
 })
